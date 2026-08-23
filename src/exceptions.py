5839c80cd82fc2904356c126d14569e425735f80
deepayan-chakravarti <deepayan.chakravarti@gmail.com>
2026-08-23 15:09:41 +0530

    fixed bugs

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,11 +1,11 @@
 import sys
 
 def error_msg_info(error, error_detail:sys):
-    exc_tb = error_detail.exc_info()
+    _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename  #extract file name from exception info
     line_no = exc_tb.tb_lineno  #extract the line no. in the file where the exception occurred
     
-    error_msg = "Error has ocurred in the python script: Name = [{0}], Line Number = [{1}], Error Message = [{2}]".format(
+    error_msg = "Error has occurred in the python script: Name = [{0}], Line Number = [{1}], Error Message = [{2}]".format(
         file_name, line_no, str(error)
     )
 
@@ -13,10 +13,17 @@ def error_msg_info(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_msg, error_detail:sys):
-        super.__init__(error_msg)
+        super().__init__(error_msg)
         self.error_msg = error_msg_info(error_msg, error_detail=error_detail)
 
     def __str__(self):
         return self.error_msg
+
+
+if __name__=="__main__":
+    try:
+        num = 1/0
+    except Exception as e:
+        raise CustomException(e, sys) from None
 
 
--- src/logger.py
+++ src/logger.py
@@ -14,3 +14,6 @@ logging.basicConfig(  #overwrites config with this convention
     format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO
 )
+
+if __name__ == "__main__":
+    logging.info("Logged a change")
